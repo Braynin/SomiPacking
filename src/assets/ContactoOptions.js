e8998e41aed63089dd4acf285fa79249b9cd142a
Gerson Matos <matosgerson661@gmail.com>
2025-01-31 18:43:41 -0500

    hasta que por fin

--- src/assets/ContactoOptions.js
+++ src/assets/ContactoOptions.js
@@ -1,24 +1,35 @@
+// assets/ContactoOptions.js
 const ContactoOptions = [
-    {
-      title: "Ubicación",
-      image:   "ubiacion.jpg",
-      description:
-        "Jirón Antonio de Elizalde 520, Lima, Perú",
-      href: "Ubicación",
-    },
-    {
-      title: "Horario",
-      image: "horario.jpg",
-      description:
-        "9:00 a. m. a 6:00 p. m.",
-      href: "Horario",
-    },
-    {
-        title: "Celular",
-        image: "celular.jpg",
-        description:
-          "924725290",
-        href: "Celular",
-      },
-  ];
-  export default ContactoOptions;
+  {
+    title: "Ubicación",
+    image: "ubiacion.jpg",
+    description: "Jirón Antonio de Elizalde 520, Lima, Perú",
+    href: "Ubicación",
+    products: [
+      { title: "Oficina 1", image: "ubiacion.jpg", description: "Ubicación de nuestra oficina principal." },
+      { title: "Oficina 2", image: "ubiacion.jpg", description: "Oficina sucursal en Lima." }
+    ],
+  },
+  {
+    title: "Horario",
+    image: "horario.jpg",
+    description: "9:00 a. m. a 6:00 p. m.",
+    href: "Horario",
+    products: [
+      { title: "Lunes a Viernes",image: "ubiacion.jpg", description: "De lunes a viernes en el horario mencionado." },
+      { title: "Fines de semana",image: "ubiacion.jpg", description: "Cerrado los fines de semana." }
+    ],
+  },
+  {
+    title: "Celular",
+    image: "celular.jpg",
+    description: "924725290",
+    href: "Celular",
+    products: [
+      { title: "Atención al cliente",image: "ubiacion.jpg", description: "Llama para consultas." },
+      { title: "Soporte técnico",image: "ubiacion.jpg", description: "Soporte en línea disponible." }
+    ],
+  },
+];
+
+export default ContactoOptions;
