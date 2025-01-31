76f484afa029fa51b4252e2f2961e186f37ffff8
Gerson Matos <matosgerson661@gmail.com>
2025-01-31 15:32:09 -0500

    holi

---
+++ src/assets/ContactoOptions.js
@@ -0,0 +1,24 @@
+const ContactoOptions = [
+    {
+      title: "Ubicación",
+      image:   "ubiacion.jpg",
+      description:
+        "Jirón Antonio de Elizalde 520, Lima, Perú",
+      href: "Ubicación",
+    },
+    {
+      title: "Horario",
+      image: "horario.jpg",
+      description:
+        "9:00 a. m. a 6:00 p. m.",
+      href: "Horario",
+    },
+    {
+        title: "Celular",
+        image: "celular.jpg",
+        description:
+          "924725290",
+        href: "Celular",
+      },
+  ];
+  export default ContactoOptions;
