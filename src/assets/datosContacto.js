b2e8f749838c8e23a3f6cdd8d53d63516349eba6
Gerson Matos <matosgerson661@gmail.com>
2025-03-27 22:37:39 -0500

    contacto casi terminado

---
+++ src/assets/datosContacto.js
@@ -0,0 +1,19 @@
+const datosContacto = [
+    {
+      title: "Horario",
+      info: "Lunes a Viernes de 9:00 a 18:00",
+      image: "../horario.jpg",
+    },
+    {
+      title: "Correo",
+      info: "team@owlyx.net",
+      image: "../mail.jpg",
+    },
+    {
+      title: "Celular",
+      info: "+51 954 659 752",
+      image: "../celular.jpg",
+    },
+  ];
+  
+  export default datosContacto;
