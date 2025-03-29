5495b93a291e413033458673e5c031fc881683b4
Braynin <brayhannb@gmail.com>
2025-03-29 17:11:04 -0500

    a versh x7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server", // Necesario para SSR en Netlify
-  adapter: netlify(),
+  output: "server",
+  adapter: vercel(),
 });
