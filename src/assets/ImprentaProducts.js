9b16e2572b16cea680d15dbe5ea073a64fc2d441
Gerson Matos <matosgerson661@gmail.com>
2025-04-02 22:57:33 -0500

    agregando mas info

--- src/assets/ImprentaProducts.js
+++ src/assets/ImprentaProducts.js
@@ -12,9 +12,10 @@ const ImprentaProducts = [
         image: "Tarjetas de Presentación.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Material: cartulina foldcote 350 gr",
+          "Impresión full color",
+          "Acabados: plastificado mate o brillante, sectorizado, relieve, hot stamping.",
+          "Personalizable"
         ],
       },
       {
@@ -22,19 +23,20 @@ const ImprentaProducts = [
         image: "Tarjeta de agradecimiento.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Material: cartulina foldcote 300 gr o 350 gr",
+          "Impresión full color",
+          "Acabados: plastificado mate o brillante, sectorizado",
+          "Personalizable."
         ],
       },
       {
-        title: "Envoltura",
+        title: "Papel de regalo / Envoltura",
         image: "Envoltura.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Material: papel bond, kraft.",
+          "Impresión offset",
+          "Personalizable",
         ],
       },
       {
@@ -42,9 +44,10 @@ const ImprentaProducts = [
         image: "Carta Comensal.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Material: cartulina foldcote",
+          "Impresión offset",
+          "Acabado: plastificado mate o brillante, sectorizado, enmicado",
+          "Personalizable"
         ],
       },
       {
@@ -62,9 +65,9 @@ const ImprentaProducts = [
         image: "Calendario.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Calendario de pared con ojales",
+          "Calendario de escritorio: encuadernado con espiral, anillado",
+          "Acabado: plastificado mate o brillante, barniz UV",
         ],
       },
       {
@@ -72,9 +75,10 @@ const ImprentaProducts = [
         image: "Individuales.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Material: papel Kraft, bond",
+          "Impresión: serigrafía, offset",
+          "Usos: restaurantes, cafeterías,catering y eventos",
+          "Personalizable",
         ],
       },
       {
@@ -82,9 +86,29 @@ const ImprentaProducts = [
         image: "Block-Agenda.png",
         description: "Bolsa de diferentes tamaños y colores.",
         caracteristicas: [
-          "Caracteristica 1",
-          "Caracteristica 2",
-          "Caracteristica 3",
+          "Impresión: offset",
+          "Acabado: plastificado mate o brillante, barniz UV",
+          "Encuadernado con espiral metálico, anillado o cosido",
+        ],
+      },
+      {
+        title: "Stickers",
+        image: "",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Material: vinilo adhesivo blanco o transparente",
+          "Impresión: offset o digital de alta resolución",
+          "Acabado: tinta resistente al agua y rayones. Plastificado mate o brillante",
+        ],
+      },
+      {
+        title: "Folders",
+        image: "",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Material: foldcote, dúplex",
+          "Impresión: offset o digital de alta resolución.",
+          "Encuadernado con espiral metálico, anillado o cosido",
         ],
       },
     ],
