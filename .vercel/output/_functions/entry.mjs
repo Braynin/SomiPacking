5d23512f6d65cb59b9c13e0fbb370bea2bb387fb
Braynin <brayhannb@gmail.com>
2025-04-07 23:24:27 -0500

    a versh

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DXI_OpVV.mjs';
-import { manifest } from './manifest_DBydf3lo.mjs';
+import { manifest } from './manifest_mO7p1Aju.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f6e9f8c9-4283-465d-8405-65cec2130612",
+    "middlewareSecret": "f3d4c43e-2b1a-4ecf-8974-74860b6af902",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
